176ef4a992d08bac32ff04c342bbac01cd32f74d
caasi Huang <caasi.igd@gmail.com>
2016-12-25 22:53:36 +0800

    Fix tonal mark JSONs

--- svg-creator/svgToJSON.js
+++ svg-creator/svgToJSON.js
@@ -3,12 +3,34 @@
 import fs from 'fs-promise'
 import path from 'path'
 import parse from 'svg-path-parser'
-import { compose, identity, filter, map, reduce } from 'ramda'
+import { compose, identity, filter, map, reduce, split } from 'ramda'
 
 
 
 const readSVG = (filepath) =>
   fs.readFile(filepath, { encoding: 'utf8' })
+
+const transformRegExp = /matrix\((.*?)\)/
+const getTransform = (svg) => {
+  let r = transformRegExp.exec(svg)
+
+  if (r) {
+    let [scaleX,,,scaleY,offsetX,offsetY] = split(',', r[1])
+    return {
+      scaleX: +scaleX,
+      scaleY: +scaleY,
+      offsetX: +offsetX,
+      offsetY: +offsetY,
+    }
+  }
+
+  return {
+    scaleX: 1.0,
+    scaleY: 1.0,
+    offsetX: 0,
+    offsetY: 0,
+  }
+}
 
 const pathRegExp = /<path\sd="(.*?)"/g
 const getPaths = (svg) => {
@@ -38,28 +60,28 @@ const pathToStroke = (path) => {
       return {
         type: 'Q',
         begin: {
-          x: path.x,
-          y: path.y,
+          x: path.x1,
+          y: path.y2,
         },
         end: {
-          x: path.x1,
-          y: path.y1,
+          x: path.x,
+          y: path.y,
         },
       }
     case 'C':
       return {
         type: 'C',
         begin: {
-          x: path.x,
-          y: path.y,
-        },
-        mid: {
           x: path.x1,
           y: path.y1,
         },
-        end: {
+        mid: {
           x: path.x2,
           y: path.y2,
+        },
+        end: {
+          x: path.x,
+          y: path.y,
         },
       }
     default:
@@ -71,7 +93,44 @@ const pathToTrack = (acc, path) => {
   return [...acc, { x: path.x, y: path.y }]
 }
 
-const toOutline = compose(filter(identity), map(pathToStroke))
+const toOutline = (trans, paths) => {
+  let ret = compose(filter(identity), map(pathToStroke))(paths)
+  ret = map(
+    c => {
+      let r = { type: c.type }
+      if (c.x) {
+        r.x = c.x * trans.scaleX + trans.offsetX
+      }
+      if (c.y) {
+        r.y = c.y * trans.scaleY + trans.offsetY
+      }
+      if (c.begin) {
+        r.begin = {}
+        r.begin = {
+          x: c.begin.x * trans.scaleX + trans.offsetX,
+          y: c.begin.y * trans.scaleY + trans.offsetY,
+        }
+      }
+      if (c.mid) {
+        r.mid = {}
+        r.mid = {
+          x: c.mid.x * trans.scaleX + trans.offsetX,
+          y: c.mid.y * trans.scaleY + trans.offsetY,
+        }
+      }
+      if (c.end) {
+        r.end = {}
+        r.end = {
+          x: c.end.x * trans.scaleX + trans.offsetX,
+          y: c.end.y * trans.scaleY + trans.offsetY,
+        }
+      }
+      return r
+    },
+    ret
+  )
+  return ret
+}
 
 const toTrack = (paths) =>
   paths.length
@@ -89,8 +148,9 @@ const main = async () => {
     async (cp) => {
       const svg = await readSVG(path.resolve(__dirname, 'svg', cp, '1.svg'))
 
+      let trans = getTransform(svg)
       let [outline, ...track] = getPaths(svg)
-      outline = toOutline(parse(outline))
+      outline = toOutline(trans, parse(outline))
       track = toTrack(map(parse, track))
 
       const word = [{ outline, track }]
